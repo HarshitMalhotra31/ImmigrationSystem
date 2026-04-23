74571d39242321724ec35c95260b7c4dfdb4473b
Harshit malhotra <harshitmalhotrashamli@gmail.com>
2026-04-23 23:50:55 +0530

    Delete ComplianceRuleProcessor.java

--- ComplianceRuleProcessor.java
+++
@@ -1,8 +0,0 @@
-package business;
-
-public class ComplianceRuleProcessor {
-    public boolean checkCompliance(String applicantName, int age) {
-        // Example rule: applicant must be over 18
-        return age >= 18;
-    }
-}
